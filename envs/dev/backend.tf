ff3a7046ceb4f1868928c9b850de7ee7aafe5143
snehalgitlab1406 <snehaldatabackup14@gmail.com>
2025-08-04 06:29:09 -0700

    Changed backend files

--- envs/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "customer-feedback-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-  }
-}
