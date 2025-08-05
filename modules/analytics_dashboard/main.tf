9db4075596cef43f13a12d72fa95ea38c25d5ee7
snehalgitlab1406 <snehaldatabackup14@gmail.com>
2025-08-05 00:50:27 -0700

    set assign public ip to true for ecs

--- modules/analytics_dashboard/main.tf
+++ modules/analytics_dashboard/main.tf
@@ -156,7 +156,7 @@ resource "aws_ecs_service" "dashboard" {
   network_configuration {
     subnets         = var.subnet_ids
     security_groups = [aws_security_group.dashboard.id]
-    assign_public_ip = false
+    assign_public_ip = true
   }
   load_balancer {
     target_group_arn = aws_lb_target_group.dashboard.arn
